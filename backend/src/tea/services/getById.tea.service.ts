2dbc93fbffc73f4e19673a5d4b5a0c97481a2058
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-10 19:15:21 -0300

    [feat]getByIdTeaService

---
+++ backend/src/tea/services/getById.tea.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { IGetByIdTeaService } from '../interfaces/service/getById.tea.service.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tea } from '../domain/tea.entity';
+import { Repository } from 'typeorm';
+import { TeaDomain } from '../domain/tea.domain';
+
+@Injectable()
+export class GetByIdTeaService implements IGetByIdTeaService {
+  constructor(@InjectRepository(Tea) private teasRepo: Repository<Tea>) {}
+
+  async getById(id: number): Promise<TeaDomain> {
+    const tea = await this.teasRepo.findOne(id);
+    return tea;
+  }
+}
