d52a5c5d78821ffbd1d7d2aa65c7d0aa7fbc9e58
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-10 19:13:14 -0300

    [feat]TeaTypes for tokens

---
+++ backend/src/tea/interfaces/types.ts
@@ -0,0 +1,12 @@
+export const TEA_TYPES = {
+  application: {
+    ICreateTeaApplication: 'ICreateTeaApplication',
+    IGetAllTeaApplication: 'IGetAllTeasApplication',
+    IGetByIdTeaApplication: 'IGetByIdTeaApplication',
+  },
+  service: {
+    ICreateTeaService: 'ICreateTeaService',
+    IGetAllTeaService: 'IGetAllTeaService',
+    IGetByIdTeaService: 'IGetByIdTeaService',
+  },
+};
