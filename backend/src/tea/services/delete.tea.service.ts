1133fefcc96803b737cf562ce3ce61a91953d6b1
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-11 23:06:30 -0300

    [feat]deleteTeaService

---
+++ backend/src/tea/services/delete.tea.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { IDeleteTeaService } from '../interfaces/service/delete.tea.service.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tea } from '../domain/tea.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class DeleteTeaService implements IDeleteTeaService {
+  constructor(@InjectRepository(Tea) private teaService: Repository<Tea>) {}
+
+  async delete(id: number): Promise<{ deleted: boolean }> {
+    try {
+      await this.teaService.delete(id);
+      return { deleted: true };
+    } catch (error) {
+      throw error;
+      return { deleted: false };
+    }
+  }
+}
