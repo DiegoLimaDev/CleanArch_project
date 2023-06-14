109b844ccac76047eb9081eb994797e0610a50f0
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-13 21:10:46 -0300

    [feat]coffee delete and update providers

--- backend/src/coffee/providers/coffee.provider.ts
+++ backend/src/coffee/providers/coffee.provider.ts
@@ -1,10 +1,14 @@
 import { CreateCoffeeApplication } from '../applications/create.coffee.application';
+import { DeleteCoffeeApplication } from '../applications/delete.coffee.application';
 import { GetAllCoffeeApplication } from '../applications/getAll.coffee.application';
 import { GetByIdCoffeeApplication } from '../applications/getById.coffee.application';
+import { UpdateCoffeeApplication } from '../applications/update.coffee.application';
 import { COFFEE_TYPES } from '../interfaces/types';
 import { CreateCoffeeService } from '../services/create.coffee.service';
+import { DeleteCoffeeService } from '../services/delete.coffee.service';
 import { GetAllCoffeeService } from '../services/getAll.coffee.service';
 import { GetByIdCoffeeService } from '../services/getById.coffee.service';
+import { UpdateCoffeeService } from '../services/update.coffee.service';
 
 const createCoffeeApp = {
   provide: COFFEE_TYPES.applications.ICreateCoffeeApplication,
@@ -17,6 +21,14 @@ const getAllCoffeeApp = {
 const getByIdCoffeeApp = {
   provide: COFFEE_TYPES.applications.IGetByIdCoffeeApplication,
   useClass: GetByIdCoffeeApplication,
+};
+const deleteCoffeeApp = {
+  provide: COFFEE_TYPES.applications.IDeleteCoffeeApplication,
+  useClass: DeleteCoffeeApplication,
+};
+const updateCoffeeApp = {
+  provide: COFFEE_TYPES.applications.IUpdateCoffeeApplication,
+  useClass: UpdateCoffeeApplication,
 };
 
 const createCoffeeService = {
@@ -31,6 +43,14 @@ const getByidCoffeService = {
   provide: COFFEE_TYPES.services.IGetByIdCoffeeService,
   useClass: GetByIdCoffeeService,
 };
+const deleteCoffeeService = {
+  provide: COFFEE_TYPES.services.IDeleteCoffeeService,
+  useClass: DeleteCoffeeService,
+};
+const updateCoffeeService = {
+  provide: COFFEE_TYPES.services.IUpdateCoffeeService,
+  useClass: UpdateCoffeeService,
+};
 
 export const coffeeProvider = [
   createCoffeeApp,
@@ -39,4 +59,8 @@ export const coffeeProvider = [
   getAllCoffeeService,
   getByIdCoffeeApp,
   getByidCoffeService,
+  deleteCoffeeApp,
+  deleteCoffeeService,
+  updateCoffeeApp,
+  updateCoffeeService,
 ];
