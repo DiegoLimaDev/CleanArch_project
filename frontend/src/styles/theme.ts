367b90a09b3921ec5b2810c18514e8c6f63e4f4f
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-13 21:23:56 -0300

    [style]theme

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    primaryColor: '#8E9882',
+    secondaryColor: '#16697A',
+    thirdColor: '#CD4C15',
+    fourthColor: '#758266',
+    auxiliar: '#C69F66',
+    auxiliar2: '#8A8370',
+    black: '#1D1D1D',
+    white: '#FBFAF8',
+    title: '#32332E',
+    subtitle: '#615F5B',
+  },
+  textSizes: {
+    small: '1.2rem',
+    medium: '1.5rem',
+    medium2: '1.8rem',
+    big: '2rem',
+    big2: '2.5rem',
+    large: '3rem',
+    extraLarge: '3.6rem',
+  },
+};
