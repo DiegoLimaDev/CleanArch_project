43e94aa71086eac574453b2370b9a0ee72a13105
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-13 21:21:57 -0300

    [feat]home page

---
+++ frontend/src/app/presentation/pages/Home.tsx
@@ -0,0 +1,32 @@
+import { Box, Stack } from '@mui/material';
+import { useAppSelector } from '../../redux/hooks';
+import { CardsDiplayMap } from '../components/CardsDiplayMap';
+import { PageTitle } from '../components/PageTitle';
+import { Text } from '../common/Text';
+import { theme } from '../../../styles/theme';
+
+export const Home = () => {
+  const { coffees } = useAppSelector((state) => state.coffeeSlice);
+  const { teas } = useAppSelector((state) => state.teaSlice);
+  const { coffeeOrTea } = useAppSelector((state) => state.coffeeOrTeaSlice);
+
+  return (
+    <Box>
+      <PageTitle />
+      {coffeeOrTea === 'coffee' && <CardsDiplayMap data={coffees} />}
+      {coffeeOrTea === 'tea' && <CardsDiplayMap data={teas} />}
+      <Stack flexDirection="row" justifyContent="center" marginBottom="5rem">
+        <Text
+          fontSize={theme.textSizes.small}
+          color={theme.colors.auxiliar2}
+          fontWeight="400"
+          textAlign="center"
+          marginRight="0.5rem"
+        >
+          Made with
+        </Text>
+        <img src="/static/ginger-cat-690 1.png" width="36px" height="17px" />
+      </Stack>
+    </Box>
+  );
+};
