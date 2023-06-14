e770ebcdbdb8a9a922c4767158795fc38b76d0eb
Diego Lima <xxdiegolsxx@gmail.com>
2023-06-13 21:08:36 -0300

    [feat]coffee delete and update Types

--- backend/src/coffee/interfaces/types.ts
+++ backend/src/coffee/interfaces/types.ts
@@ -3,10 +3,14 @@ export const COFFEE_TYPES = {
     ICreateCoffeeApplication: 'ICreateCoffeeApplication',
     IGetAllCoffeeApplication: 'IGetAllCoffeeApplication',
     IGetByIdCoffeeApplication: 'IGetByIdCoffeeApplication',
+    IDeleteCoffeeApplication: 'IDeleteCoffeeApplication',
+    IUpdateCoffeeApplication: 'IUpdateCoffeeApplication',
   },
   services: {
     ICreateCoffeeService: 'ICreateCoffeeService',
     IGetAllCoffeeService: 'IGetAllCoffeeService',
     IGetByIdCoffeeService: 'IGetByIdCoffeeService',
+    IDeleteCoffeeService: 'IDeleteCoffeeService',
+    IUpdateCoffeeService: 'IUpdateCoffeeService',
   },
 };
